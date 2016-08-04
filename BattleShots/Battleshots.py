763d08c0039a984d158d6642eb3b2d93b4874175
ChristopherLindberg <Christopherwlindberg@gmail.com>
2016-08-04 12:45:49 +0200

    Changed how hit fields were saved

--- BattleShots/Battleshots.py
+++ BattleShots/Battleshots.py
@@ -4,7 +4,7 @@ from os import startfile,system #For system control
 
 
 def getCoordinate(minTime,maxTime): #waits a number of seconds, finds a random coordinate, checks if this coordinate has already been hit, if not it hits it
-    time = uniform(minTime,maxTime) * 5 #60 to get time from seconds to minutes; time is the amount of time to wait
+    time = uniform(minTime,maxTime) #"time" is the amount of time to wait
     sleep(time) #waits for "time" seconds
     
     while True: #run until a coordinate which has not been hit before has been hit
@@ -19,63 +19,117 @@ def getCoordinate(minTime,maxTime): #waits a number of seconds, finds a random c
                 break; #breaks the for loop since the coordinate has already been hit
             
         if(notAlreadyHit): #if the coordinate has not been hit, call airstrike to display what's going to happen and play sound
+#            system('CLS') #clear screen - equal to matlab cls        
+            saveCoordinate,clusterBomb = effectValue()            
             airStrike(coordinate_Letters,coordinate_Numbers)
             
-            used_Coordinates_Numbers.append(coordinate_Numbers) #add to already hit numbers,letters
-            used_Coordinates_Letters.append(coordinate_Letters)
+            if(saveCoordinate):
+                used_Coordinates_Numbers.append(coordinate_Numbers) #add to already hit numbers,letters
+                used_Coordinates_Letters.append(coordinate_Letters)
+                
+                
+            coordinate_Letters = 'J'
+            coordinate_Numbers = 1
+                
+            if(clusterBomb):
+                
+            #even though the try except block works. It will sometimes give a result due to pythons negative index of lists.
+            #To avoid that the index have been set to positive by the two if statements below.
+            #This is done since the negative index can at most be -9 so setting this to 0 just shifts the for 
+            #loop to the right which is countered by addIndex being one less. i.e.
+            #it goes from -9 to +1 to 0 to +2 by the if statements and then addindex makes it 0 to +1 which is desired.
+                
+                addIndexNumber = 2; #for python negative index 
+                addIndexLetter = 2; #for python negative index 
+                if(letters.index(coordinate_Letters)-1 < 0): #to remove the negative index in python
+                    coordinate_Letters = letters[letters.index(coordinate_Letters)+1];
+                    addIndexLetter = 1;
+                if(numbers.index(coordinate_Numbers)-1 < 0): #to remove the negative index in python
+                    coordinate_Numbers = numbers[numbers.index(coordinate_Numbers)+1];
+                    addIndexNumber = 1
+                    
+                    
+                for i in range(letters.index(coordinate_Letters)-1,letters.index(coordinate_Letters)+addIndexLetter):
+                    for j in range(numbers.index(coordinate_Numbers)-1,numbers.index(coordinate_Numbers)+addIndexNumber):
+                        try: 
+                            numbers[j] #does it exist
+                            letters[i] #does it exist
+                            used_Coordinates_Numbers.append(numbers[j])
+                            used_Coordinates_Letters.append(letters[i])
+                        except:
+                            pass
+                
             break;
 
 def effectValue(): #An effect is chosen
-    higher_value=99
+    higher_value=100
     lower_value=1
     final_value = randint(lower_value, higher_value)
-    #print(final_value)
-    if 0 < final_value < 6:
-        print('Cluster Bomb!')
-        effect='a'
-    elif 5 < final_value < 41:
-        print('Airstrike!')
-        effect='b'
-    elif 40 < final_value < 51:
-        print('Heat seeking missile!')
-        effect='c'
-    elif 50 < final_value < 61:
-        print('Nothing happens')
-        effect='d'
-    elif 60 < final_value < 81:
-        print('Fællesskål!')
-        effect='e'
-    elif 80 < final_value < 96:
-        print('Shots!')
-        effect='f'
-    else: #ranges from 96 to 99; 96 < final_value < 100
-        print('something else!')
-        effect='g'
-    return effect
+    saveCoordinate = False # do not save the coordinate where it hit
+    clusterBomb = False;
 
-def airStrike(letter,number): #display sounds and message
-    system('CLS') #clear screen - equal to matlab cls
-    print("(" + letter + "," + str(number) + ")") #print coordinate
-    
-    if effectValue() == 'c':
+    if 1 <= final_value <= 5: #5 procent
+        print('Cluster Bomb!')
+        clusterBomb = True;
+        
+        
+    elif 6 <= final_value <= 35: #30 procent
+        print('Airstrike!')
+        saveCoordinate = True; # do not save the coordinate     
+        
+        
+    elif 36 <= final_value <= 45: # 10 procent
+        print('Heat seeking missile!')
         Player=choice(PlayerPool)
         print(Player + " is awarded a heat seeking missile!")
-    startfile('Airstrike+sound+effect.mp3')# play sound
+        
+        
+    elif 46 <= final_value <= 55: #10 procent
+        print('Strongest player drinks 5!') 
+        
+        
+    elif 56 <= final_value <= 70: #15 procent
+        print('Fællesskål!')
+        
+        
+    elif 71 <= final_value <= 80: #10 procent
+        print('A shot!')
+        
+        
+    else: #ranges from 81 <= final_value <= 100 #20 procent
+        print('If water was hit you have to drink 5');
+        print("If a ship was hit you have to drink 5");
+        print("If a ship was hit, but the ship had already been hit before you can give away 3");
+        
+        
+    return saveCoordinate,clusterBomb
+
+def airStrike(letter,number): #display sounds and message
+    print("(" + letter + "," + str(number) + ")") #print coordinate
+#    startfile('Airstrike+sound+effect.mp3')# play sound
     
     
-PlayerPool = ();
-numberPlayers = int(input("Enter Number of players "))
-for i in range(numberPlayers):
+PlayerPool = (); #init
+numberPlayers = int(input("Enter Number of players! "))
+
+for i in range(numberPlayers): #keep adding players
     x = (input("Enter player name "),);
     PlayerPool +=x;
-    
+system('CLS') #clear screen - equal to matlab cls      
 letters = ('A','B','C','D','E','F','G','H','I','J'); #possible letters
 numbers = (1,2,3,4,5,6,7,8,9,10); #possible numbers
 used_Coordinates_Numbers = []; # coordinates which have already been hit
 used_Coordinates_Letters = []; 
 
-minTime = 0; #min wait time
-maxTime = 1; #max wait time
+minMinutes = int(input("Enter minimum minutes to wait")) #minimum minutes to wait
+minSeconds = int(input("Enter minimum seconds to wait")) #minimum seconds to wait
+
+maxMinutes = int(input("Enter maximum minutes to wait")) #maximum minutes to wait
+maxSeconds = int(input("Enter maximum seconds to wait")) #maximum seconds to wait
+
+minTime = 60*minMinutes + minSeconds; #min wait time in seconds
+
+maxTime = 60*maxMinutes + maxSeconds; #max wait time in seconds
 
 while True: #keep running until game is done
     getCoordinate(minTime,maxTime)
